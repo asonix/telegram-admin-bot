893fd142e5c8407dd8e03ad53eec38561487cb1b
Riley Trautman <asonix.dev@gmail.com>
2017-11-20 17:32:08 -0600

    Do things on desktop

--- arm-eabihf.sh
+++ arm-eabihf.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
 
-export RUSTFLAGS='-C target-feature=-crt-static -L native=/usr/arm-linux-gnueabihf/lib -L native=/home/asonix/Development/armv7h/lib'
-export OPENSSL_DIR=/home/asonix/Development/armv7h
+export RUSTFLAGS='-C target-feature=-crt-static -L native=/usr/arm-linux-gnueabihf/lib -L native=/home/asonix/Development/armeabi/lib'
+export OPENSSL_DIR=/home/asonix/Development/armeabi
 export PKG_CONFIG_ALLOW_CROSS=1
 cargo build --target arm-unknown-linux-gnueabihf --release
